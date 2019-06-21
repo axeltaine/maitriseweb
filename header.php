a795a66ce56114b6ae25b3dffa0e8ecea8a2ba9e
JeremyMarchewka <j.marchewka@simplon-charleville.fr>
2019-06-21 19:21:57 +0200

    + portfolio

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-custom navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-custom navbar-expand-lg navbar-light">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
         style="outline: none;">
@@ -7,7 +7,7 @@
         </span>
     </button>
     <h2 class="nav-title" style="color:#eee">Maitrise WEB</h2>
-    <a class="navbar-brand2" href="#section1">
+    <a class="navbar-brand2" href="index.php">
         <h1><img class="logo2" src="img/logo_transpa.png" alt="logo"></h1>
     </a>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -21,12 +21,12 @@
                 <a class="nav-link" href="#section2">Présentation</a>
             </li>
 
-            <a class="navbar-brand" href="#section1">
+            <a class="navbar-brand" href="index.php">
                 <h1><img class="logo" src="img/logo_transpa.png" alt="logo"></h1>
             </a>
 
             <li class="nav-item hvr-grow-shadow hvr-underline-from-center">
-                <a class="nav-link" href="#section3">Réalisations</a>
+                <a class="nav-link" href="portfolio.php">Réalisations</a>
             </li>
             <li class="nav-item hvr-grow-shadow hvr-underline-from-center">
                 <a class="nav-link" href="#section4">Contact</a>
@@ -34,15 +34,3 @@
         </ul>
     </div>
 </nav>
-
-
-
-<script type="text/javascript">
-    $(window).on("scroll", function () {
-        if ($(window).scrollTop()) {
-            $('nav').addClass('black');
-        } else {
-            $('nav').removeClass('black');
-        }
-    })
-</script>
