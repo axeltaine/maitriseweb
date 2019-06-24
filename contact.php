76e16b7cc2ff6b6ecd3fd4494bba10616a507d90
axel taine <axeltaine@MacBook-Pro-de-axel-taine.local>
2019-06-24 14:22:06 +0200

    contact

--- contact.php
+++ contact.php
@@ -7,12 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>contact</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-
+    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.min.css'>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
         integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+        <link href="contact.css" rel="stylesheet">
+        <link href="membre.css" rel="stylesheet">
+        
     <link href="css/style.css" rel="stylesheet">
     <link href="css/header.css" rel="stylesheet">
     <link href="css/hover-min.css" rel="stylesheet">
@@ -20,61 +22,52 @@
 
 <body>
     <header><?php include ('header.php'); ?> </header>
-    <style>#contact {
-  padding-top: 45px;
-  padding-bottom: 0;
-}
-
-#google-map {
-  height: 350px;
-}
-
-#contact-us {
-  background-color:black;
-  padding-bottom: 90px; 
-}
-
-#contact-us .heading {
-  padding-top: 95px;
-}
-
-.form-control {
-  background-color: transparent;
-  border-color: rgba(255,255,255,.5);
-  height: 50px;
-  border-radius: 0;
-  box-shadow: none;
-}
-
-textarea.form-control {
-  min-height: 180px;
-  resize:none;
-}
-
-.form-group {
-  margin-bottom: 30px;
-}
-
-.contact-info {
-  padding-left:70px;
-  font-weight: 300;
-}
-
-ul.address {
-  margin-top: 30px;
-  list-style: none;
-  padding: 0;
-  margin: 0;
-}
-
-.contact-info ul li {
-  margin-bottom: 8px;
-}
-
-.contact-info ul li a {
-  color: #fff;
-}</style>
+    
+    <div id="top"></div>
     <main class="global">
+        <div class="membre">
+        <div class="container m-t-md">
+            <H2>Notre équipe :</H2>
+  <!-- First row -->
+  <div class="row">
+    <div class="col-xs-12 col-md-4">
+      <!-- Card -->
+      <article class="card animated fadeInLeft">
+        <img class="card-img-top img-responsive" src="img/19105875_10211933330962903_8610926667809294383_n.jpg" style="max-height: 10em; "alt="Deer in nature" />
+        <div class="card-block">
+          <h4 class="card-title">Animal Farm</h4>
+          <p class="card-text">Tired of their servitude to man, a group of farm animals revolt and establish their own society, only to be betrayed into worse servitude by their leaders, the pigs. This satire addresses the communist philosophy the Soviet Union.</p>
+          
+        </div>
+      </article><!-- .end Card -->
+    </div>
+    <div class="col-xs-12 col-md-4">
+      <!-- Card -->
+      <article class="card animated fadeInUp">
+        <img class="card-img-top img-responsive" src="img/19105875_10211933330962903_8610926667809294383_n.jpg" style="max-height: 10em; " alt="Deer in nature" />
+        <div class="card-block">
+          <h4 class="card-title">Animal Farm</h4>
+          <p class="card-text">Tired of their servitude to man, a group of farm animals revolt and establish their own society, only to be betrayed into worse servitude by their leaders, the pigs. This satire addresses the communist philosophy the Soviet Union.</p>
+          
+        </div>
+      </article>
+      <!-- .end Card -->
+    </div>
+    <div class="col-xs-12 col-md-4">
+      <!-- Card -->
+      <article class="card animated fadeInRight">
+        <img class="card-img-top img-responsive" src="img/19105875_10211933330962903_8610926667809294383_n.jpg" style="max-height: 10em; " alt="Deer in nature" />
+        <div class="card-block">
+          <h4 class="card-title">Animal Farm</h4>
+          <p class="card-text">Tired of their servitude to man, a group of farm animals revolt and establish their own society, only to be betrayed into worse servitude by their leaders, the pigs. This satire addresses the communist philosophy the Soviet Union.</p>
+          
+        </div>
+      </article>
+      <!-- .end Card -->
+    </div>
+  </div><!-- .end First row -->
+</div><!-- .end First row -->
+</div>
         <section id="contact">
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2576.9561940697363!2d4.720275315706772!3d49.768080979387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ea0e062e03d7e9%3A0xc5cc5f6ee91f2b25!2sCCI+des+Ardennes!5e0!3m2!1sfr!2sfr!4v1553546933236"
@@ -85,7 +78,7 @@ ul.address {
                         <div class="heading text-center col-sm-8 col-sm-offset-2 wow fadeInUp"
                             data-wow-duration="1000ms" data-wow-delay="300ms">
                             <h2>Contact</h2>
-                            <p>Envoyer moi un message.</p>
+                            <p>Envoyer nous un message.</p>
                         </div>
                     </div>
                     <div class="contact-form wow fadeIn" data-wow-duration="1000ms" data-wow-delay="600ms">
@@ -142,6 +135,7 @@ ul.address {
         </section>
     </main>
     <footer><?php include ('footer.html'); ?></footer>
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
