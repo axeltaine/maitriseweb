826096d5faa831d84cb7fafbfc2ce9db32b6f625
JeremyMarchewka <j.marchewka@simplon-charleville.fr>
2019-06-24 16:35:39 +0200

    footer

--- portfolio.php
+++ portfolio.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>MAitrise Web - Portfolio</title>
+	<title>Maitrise Web - Portfolio</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
 		integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
