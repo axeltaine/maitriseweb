a9b3185ab2e5c57a762a109f77733e2a69ff68f3
axel taine <axeltaine@MacBook-Pro-de-axel-taine.local>
2019-06-25 16:31:20 +0200

    contact

--- contact.php
+++ contact.php
@@ -12,7 +12,7 @@
         integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link href="contact.css" rel="stylesheet">
+    <link href="contact.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
     <link href="css/header.css" rel="stylesheet">
     <link href="css/hover-min.css" rel="stylesheet">
@@ -21,26 +21,25 @@
 <body>
     <header><?php include ('header.php'); ?> </header>
     <div class="header ">
-		<h1>MAitrise Web <br><span>[ Contact ]</span></h1>
-	</div>
+        <h1>MAitrise Web <br><span>[ Contact ]</span></h1>
+    </div>
     <div id="top"></div>
     <main class="global">
-    <div class="container_fluide" id="section2">
+        <div class="container_fluide" id="section2">
             <div class="jumbotron text-center">
                 <div class="container">
-                    <h2 class="jumbotron-heading">Une question ?</h1><br>
-                    <h2 class="jumbotron-heading">Un projet ?</h1><br>  
+                    <h1 class="jumbotron-heading">Une question ?</h1><br>
+                        <h1 class="jumbotron-heading">Un projet ?</h1><br>
                 </div>
             </div>
         </div>
         <section id="contact">
-            
             <div id="contact-us">
                 <div class="container">
                     <div class="row">
-                        <div class="heading text-center col-sm-12 col-sm-offset-2  fadeInUp"
-                            data-wow-duration="1000ms" data-wow-delay="300ms">
-                            
+                        <div class="heading text-center col-sm-12 col-sm-offset-2  fadeInUp" data-wow-duration="1000ms"
+                            data-wow-delay="300ms">
+
                             <p>Envoyer nous un message.</p>
                         </div>
                     </div>
@@ -62,10 +61,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="form-group">
-                                        <input type="text" name="subject" class="form-control" placeholder="Subject"
-                                            required="required">
-                                    </div>
+                                    
                                     <div class="form-group">
                                         <textarea name="message" id="message" class="form-control" rows="4"
                                             placeholder="Enter your message" required="required"></textarea>
@@ -75,19 +71,58 @@
                                     </div>
                                 </form>
                             </div>
-                           
                         </div>
                     </div>
                 </div>
             </div>
         </section>
- 
-        <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2576.9561940697363!2d4.720275315706772!3d49.768080979387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ea0e062e03d7e9%3A0xc5cc5f6ee91f2b25!2sCCI+des+Ardennes!5e0!3m2!1sfr!2sfr!4v1553546933236"
-                width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <section id="adresse">
+            <div id="contact-us">
+                <div class="container">
+                    <div class="row">
+                        <div class="heading text-center col-sm-12 col-sm-offset-2  fadeInUp" data-wow-duration="1000ms"
+                            data-wow-delay="300ms">
+                            <p>Notre agence est là pour vous</p>
+                        </div>
+                    </div>
+                    <div class="contact-form  fadeIn" data-wow-duration="1000ms" data-wow-delay="600ms">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <form id="main-contact-form" name="contact-form" method="post" action="#">
+                                    <div class="row   fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+                                        <div class="col-sm-12">
+                                            <div class="form-group">
+                                            <address class="contact-address">
+                            <span class="contactAddress-title">PARTOUT AILLEURS</span><br>
+                            18 A Avenue bleleldlel
+                            <span class="contact-address-city">
+                                08000 Charleville-Mézières
+                            </span>
+                        </address>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-12">
+                                            <div class="form-group">
+                                                <iframe
+                                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2576.9561940697363!2d4.720275315706772!3d49.768080979387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ea0e062e03d7e9%3A0xc5cc5f6ee91f2b25!2sCCI+des+Ardennes!5e0!3m2!1sfr!2sfr!4v1553546933236"
+                                                    width="100%" height="300" frameborder="0" style="border:0"
+                                                    allowfullscreen></iframe>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="row">
+            <img class="img-fluid" alt="Responsive image" src="img/1624.jpg">
+        </div>
     </main>
     <footer><?php include ('footer.html'); ?></footer>
-    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
