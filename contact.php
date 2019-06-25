5069de3b5c795687d860373ea5b9144754858cef
axel taine <axeltaine@MacBook-Pro-de-axel-taine.local>
2019-06-25 14:28:50 +0200

    contact

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link href="contact.css" rel="stylesheet">
-        <link href="membre.css" rel="stylesheet">
+        
         
     <link href="css/style.css" rel="stylesheet">
     <link href="css/header.css" rel="stylesheet">
@@ -27,23 +27,30 @@
 	</div>
     <div id="top"></div>
     <main class="global">
-       
+    <div class="container_fluide" id="section2">
+            <div class="jumbotron text-center">
+                <div class="container">
+                    <h2 class="jumbotron-heading">Une question ?</h1><br>
+                    <h2 class="jumbotron-heading">Un projet ?</h1><br>  
+                </div>
+            </div>
+        </div>
         <section id="contact">
             
-            <div id="contact-us" class="parallax">
+            <div id="contact-us">
                 <div class="container">
                     <div class="row">
-                        <div class="heading text-center col-sm-8 col-sm-offset-2 wow fadeInUp"
+                        <div class="heading text-center col-sm-12 col-sm-offset-2  fadeInUp"
                             data-wow-duration="1000ms" data-wow-delay="300ms">
-                            <h2>Contact</h2>
+                            
                             <p>Envoyer nous un message.</p>
                         </div>
                     </div>
-                    <div class="contact-form wow fadeIn" data-wow-duration="1000ms" data-wow-delay="600ms">
+                    <div class="contact-form  fadeIn" data-wow-duration="1000ms" data-wow-delay="600ms">
                         <div class="row">
-                            <div class="col-sm-6">
+                            <div class="col-sm-12">
                                 <form id="main-contact-form" name="contact-form" method="post" action="#">
-                                    <div class="row  wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+                                    <div class="row   fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
                                         <div class="col-sm-6">
                                             <div class="form-group">
                                                 <input type="text" name="name" class="form-control" placeholder="Name"
@@ -70,27 +77,23 @@
                                     </div>
                                 </form>
                             </div>
-                            <div class="col-sm-6">
-                                <div class="contact-info wow fadeInUp" data-wow-duration="1000ms"
-                                    data-wow-delay="300ms">
-                                    <ul class="address">
-                                        <li><i class="fa fa-map-marker"></i> <span> Address:</span> 18 A Avenue Georges
-                                            Corneau, 08000
-                                            Charleville-Mézières </li>
-                                        <li><i class="fa fa-phone"></i> <span> Phone:</span> 06.51.47.16.78 </li>
-                                        <li><i class="fa fa-envelope"></i> <span> Email:</span><a
-                                                href="a.taine@simplon-charleville.fr">
-                                                a.taine@simplon-charleville.fr</a></li>
-                                        <li><i class="fa fa-globe"></i> <span> Website:</span> <a
-                                                href="#">www.taine.simplon-charleville.fr</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+                           
                         </div>
                     </div>
                 </div>
             </div>
         </section>
+        <div class="container">
+  <div class="row">
+    <div class="col-sm-6">
+      One of three columns
+    </div>
+    <div class="col-sm-6">
+      One of three columns
+    </div>
+    
+  </div>
+</div>
         <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2576.9561940697363!2d4.720275315706772!3d49.768080979387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ea0e062e03d7e9%3A0xc5cc5f6ee91f2b25!2sCCI+des+Ardennes!5e0!3m2!1sfr!2sfr!4v1553546933236"
                 width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
