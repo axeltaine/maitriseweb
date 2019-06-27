6e8325513c69955c75f74dd24bde9ba57043148a
JeremyMarchewka <j.marchewka@simplon-charleville.fr>
2019-06-27 13:47:03 +0200

    stripes include

--- index.php
+++ index.php
@@ -57,38 +57,6 @@
     </script>
     <script type="text/javascript" src="slider/revolution/js/extensions/revolution.extension.video.min.js"></script>
 
-    <style>
-        .stripes-card {
-            height: 100vh;
-            padding: 0;
-            margin: 3% 0;
-            display: flex;
-        }
-
-        .stripes {
-            height: 100%;
-            width: 25%;
-            padding: 0;
-            cursor: pointer;
-            overflow: hidden
-        }
-
-        .stripes-content {
-            height: 100%;
-            overflow: hidden;
-        }
-
-        .stripes-img {
-            height: 100%;
-            width: 100%;
-            object-fit: cover;
-        }
-        
-        .stripes-img:hover {
-            transform: scale(1.2);
-            transition: all 5s ease;
-        }
-    </style>
 
 </head>
 
@@ -106,37 +74,8 @@
             </div>
         </div>
         <div class="container_fluide"><?php include ('slider.html'); ?></div>
-        <div class="stripes-card">
-            <div class="stripes">
-                <div class="stripes-content">
-                    <img class="stripes-img" src="img/38018499_1520923464675970_2235721715281821696_o.jpg" alt="">
-                    <h3 style="position: relative; z-index: 30; margin-top: -550px; text-align: center; ">Sites vitrine</h3>
-                    <p style="position: relative; z-index: 30; text-align: center; ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi aperiam facilis dolor vero exercitationem. Soluta, sunt.</p>
-                </div>
-            </div>
-            <div class="stripes">
-                <div class="stripes-content">
-                    <img class="stripes-img" src="img/37775430_1508245949277055_4923942506057433088_o.jpg" alt="">
-                     <h3 style="position: relative; z-index: 30; margin-top: -550px; text-align: center; ">Sites vitrine</h3>
-                    <p style="position: relative; z-index: 30; text-align: center; ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi aperiam facilis dolor vero exercitationem. Soluta, sunt.</p>
-                </div>
-            </div>
-            <div class="stripes">
-                <div class="stripes-content">
-                    <img class="stripes-img" src="img/37854358_1508245979277052_1686911270940311552_o.jpg" alt="">
-                     <h3 style="position: relative; z-index: 30; margin-top: -550px; text-align: center; ">Sites vitrine</h3>
-                    <p style="position: relative; z-index: 30; text-align: center; ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi aperiam facilis dolor vero exercitationem. Soluta, sunt.</p>
-                </div>
-            </div>
-            <div class="stripes">
-                <div class="stripes-content">
-                    <img class="stripes-img" src="img/49608100_1716725738429074_4809086061131071488_o.jpg" alt="">
-                     <h3 style="position: relative; z-index: 30; margin-top: -550px; text-align: center; ">Sites vitrine</h3>
-                    <p style="position: relative; z-index: 30; text-align: center; ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi aperiam facilis dolor vero exercitationem. Soluta, sunt.</p>
-                </div>
-            </div>
-        </div>
-
+        
+        <?php include ('stripes.php'); ?>
         <?php include ('parallax.html'); ?>
         <?php include ('gallery.html'); ?>
         <section class="testimonial text-center">
---
+++ stripes.php
@@ -0,0 +1,65 @@
+
+    <style>
+        .stripes-card {
+            height: 100vh;
+            padding: 0;
+            margin: 3% 0;
+            display: flex;
+        }
+
+        .stripes {
+            height: 100%;
+            width: 25%;
+            padding: 0;
+            cursor: pointer;
+            overflow: hidden
+        }
+
+        .stripes-content {
+            height: 100%;
+            overflow: hidden;
+        }
+
+        .stripes-img {
+            height: 100%;
+            width: 100%;
+        }
+        
+        .stripes-img:hover {
+            transform: scale(1.2);
+            transition: all 5s ease;
+        }
+    </style>
+
+    
+<h2 style="text-align: center; text-transform: uppercase; margin: 2% 0;">créations</h2>
+        <div class="stripes-card">
+            <div class="stripes">
+                <div class="stripes-content">
+                    <img class="stripes-img" src="img/38018499_1520923464675970_2235721715281821696_o.jpg" alt="">
+                    <h3 style="position: relative; z-index: 30; margin-top: -750px; text-align: center; ">Sites vitrine</h3>
+                    <p style="position: relative; z-index: 30; text-align: center; ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi aperiam facilis dolor vero exercitationem. Soluta, sunt.</p>
+                </div>
+            </div>
+            <div class="stripes">
+                <div class="stripes-content">
+                    <img class="stripes-img" src="img/37775430_1508245949277055_4923942506057433088_o.jpg" alt="">
+                     <h3 style="position: relative; z-index: 30; margin-top: -750px; text-align: center; ">Sites vitrine</h3>
+                    <p style="position: relative; z-index: 30; text-align: center; ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi aperiam facilis dolor vero exercitationem. Soluta, sunt.</p>
+                </div>
+            </div>
+            <div class="stripes">
+                <div class="stripes-content">
+                    <img class="stripes-img" src="img/37854358_1508245979277052_1686911270940311552_o.jpg" alt="">
+                     <h3 style="position: relative; z-index: 30; margin-top: -750px; text-align: center; ">Sites vitrine</h3>
+                    <p style="position: relative; z-index: 30; text-align: center; ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi aperiam facilis dolor vero exercitationem. Soluta, sunt.</p>
+                </div>
+            </div>
+            <div class="stripes">
+                <div class="stripes-content">
+                    <img class="stripes-img" src="img/49608100_1716725738429074_4809086061131071488_o.jpg" alt="">
+                     <h3 style="position: relative; z-index: 30; margin-top: -750px; text-align: center; ">Sites vitrine</h3>
+                    <p style="position: relative; z-index: 30; text-align: center; ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi aperiam facilis dolor vero exercitationem. Soluta, sunt.</p>
+                </div>
+            </div>
+        </div>
