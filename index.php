34a542f4ab4b02fe0db08c653da52b34079fc88d
axel taine <axeltaine@MacBook-Pro-de-axel-taine.local>
2019-06-27 14:34:13 +0200

    modif index +include

--- index.php
+++ index.php
@@ -76,61 +76,14 @@
         <div class="container_fluide"><?php include ('slider.html'); ?></div>
         
         <?php include ('stripes.php'); ?>
-        <?php include ('parallax.html'); ?>
         <?php include ('gallery.html'); ?>
-        <section class="testimonial text-center">
-            <div class="container">
-                <div class="heading white-heading">
-                    Ils nous on fait confiance :
-                </div>
-                <div id="testimonial4"
-                    class="carousel slide testimonial4_indicators testimonial4_control_button thumb_scroll_x swipe_x"
-                    data-ride="carousel" data-pause="hover" data-interval="5000" data-duration="2000">
-                    <div class="carousel-inner" role="listbox">
-                        <div class="carousel-item active">
-                            <div class="testimonial4_slide">
-                                <img src="https://i.ibb.co/8x9xK4H/team.jpg" class="img-circle img-responsive" />
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                                    Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                                    unknown printer took a galley of type and scrambled it to make a type specimen book.
-                                </p>
-                                <h4>Client 1</h4>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="testimonial4_slide">
-                                <img src="https://i.ibb.co/8x9xK4H/team.jpg" class="img-circle img-responsive" />
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                                    Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                                    unknown printer took a galley of type and scrambled it to make a type specimen book.
-                                </p>
-                                <h4>Client 2</h4>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="testimonial4_slide">
-                                <img src="https://i.ibb.co/8x9xK4H/team.jpg" class="img-circle img-responsive" />
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                                    Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                                    unknown printer took a galley of type and scrambled it to make a type specimen book.
-                                </p>
-                                <h4>Client 3</h4>
-                            </div>
-                        </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#testimonial4" data-slide="prev">
-                        <span class="carousel-control-prev-icon"></span>
-                    </a>
-                    <a class="carousel-control-next" href="#testimonial4" data-slide="next">
-                        <span class="carousel-control-next-icon"></span>
-                    </a>
-                </div>
-            </div>
-        </section>
+        <?php include ('parallax.html'); ?>
+        
+        
         <div class="container_fluide" id="section4">
             <div class="jumbotron text-center">
                 <div class="container">
-                    <h1 class="jumbotron-heading">CONTACT</h1>
+                   
                     <p class="lead text-muted">Un projet en tête ? venez nous en faire part. MAITRISE WEB répondra a vos
                         besoins en vous écoutant.</p>
                     <p class="lead">
@@ -141,7 +94,7 @@
                 </div>
             </div>
         </div>
-
+<?php include ('testimodial.php'); ?>
     </main>
 
     <footer><?php include ('footer.html'); ?></footer>
