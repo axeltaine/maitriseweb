ca8c02339fcc4772dd867327fe11fcd515967050
JeremyMarchewka <j.marchewka@simplon-charleville.fr>
2019-06-27 13:19:35 +0200

    maj2 stripes

--- index.php
+++ index.php
@@ -58,12 +58,37 @@
     <script type="text/javascript" src="slider/revolution/js/extensions/revolution.extension.video.min.js"></script>
 
     <style>
-        .truc:hover {
-            transform: scale(1.1);
-            transition: 0.5s ease;
+        .stripes-card {
+            height: 100vh;
+            padding: 0;
+            margin: 3% 0;
+            display: flex;
+        }
+
+        .stripes {
+            height: 100%;
+            width: 25%;
+            padding: 0;
+            cursor: pointer;
+            overflow: hidden
+        }
+
+        .stripes-content {
+            height: 100%;
+            overflow: hidden;
+        }
+
+        .stripes-img {
+            height: 100%;
+            width: 100%;
+        }
+        
+        .stripes-img:hover {
+            transform: scale(1.2);
+            transition: 2s ease-in-out;
         }
     </style>
-        
+
 </head>
 
 <body>
@@ -80,24 +105,29 @@
             </div>
         </div>
         <div class="container_fluide"><?php include ('slider.html'); ?></div>
-
-
-
-        <div class="bidule" style="height: 100vh; padding: 0; margin: 3% 0; display: flex; overflow: hidden ">
-
-            <div class="truc" style="height: 100%; width: 25%; padding: 0; cursor: pointer;"><img
-                    style="height: 100%; width: 100%; overflow: hidden;     object-fit: cover;"
-                    src="img/38018499_1520923464675970_2235721715281821696_o.jpg" alt=""></div>
-            <div class="truc" style="height: 100%; width: 25%; padding: 0; cursor: pointer;"><img
-                    style="height: 100%; width: 100%; overflow: hidden;     object-fit: cover;"
-                    src="img/37775430_1508245949277055_4923942506057433088_o.jpg" alt=""></div>
-            <div class="truc" style="height: 100%; width: 25%; padding: 0; cursor: pointer;"><img
-                    style="height: 100%; width: 100%; overflow: hidden;     object-fit: cover;"
-                    src="img/37854358_1508245979277052_1686911270940311552_o.jpg" alt=""></div>
-            <div class="truc" style="height: 100%; width: 25%; padding: 0; cursor: pointer;"><img
-                    style="height: 100%; width: 100%; overflow: hidden;     object-fit: cover;"
-                    src="img/49608100_1716725738429074_4809086061131071488_o.jpg" alt=""></div>
-
+        <div class="stripes-card">
+            <div class="stripes">
+                <div class="stripes-content">
+                    <img class="stripes-img" src="img/38018499_1520923464675970_2235721715281821696_o.jpg" alt="">
+                    <h3 style="position: relative; z-index: 30; margin-top: -550px; text-align: center; ">Sites vitrine</h3>
+                    <p style="position: relative; z-index: 30; text-align: center; ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi aperiam facilis dolor vero exercitationem. Soluta, sunt.</p>
+                </div>
+            </div>
+            <div class="stripes">
+                <div class="stripes-content">
+                    <img class="stripes-img" src="img/37775430_1508245949277055_4923942506057433088_o.jpg" alt="">
+                </div>
+            </div>
+            <div class="stripes">
+                <div class="stripes-content">
+                    <img class="stripes-img" src="img/37854358_1508245979277052_1686911270940311552_o.jpg" alt="">
+                </div>
+            </div>
+            <div class="stripes">
+                <div class="stripes-content">
+                    <img class="stripes-img" src="img/49608100_1716725738429074_4809086061131071488_o.jpg" alt="">
+                </div>
+            </div>
         </div>
 
         <?php include ('parallax.html'); ?>
