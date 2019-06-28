561ea6d7253ffd7cfb232d5ffe2726a45d52ef87
axel taine <axeltaine@MacBook-Pro-de-axel-taine.local>
2019-06-28 13:59:56 +0200

    modif btt

--- presentation.php
+++ presentation.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>contact</title>
+  <title>Présentation</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
