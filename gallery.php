269da8e5cc051141404524253fe638ad963a44f7
JeremyMarchewka <j.marchewka@simplon-charleville.fr>
2019-07-01 13:50:53 +0200

    ajout modal sur index

--- gallery.php
+++ gallery.php
@@ -1,3 +1,4 @@
+<div id="top" style="margin-bottom: 80px;"></div>
 <hr>
 <h2 style="text-align: center; text-transform: uppercase; margin: 2% 0;">NOS PROJETS & CREATIONS</h2>
 <hr>
@@ -32,42 +33,46 @@
 						<button class="dropdown-item" type="button">Site Web</button>
 						<button class="dropdown-item" type="button">Flyer</button>
 						<button class="dropdown-item" type="button">Identité visuelle</button>
-						<button class="dropdown-item" type="button">Projet en cours</button>
+						<button class="dropdown-item" type="button">Projet en 
 					</div>
 				</div>
 			</div>
+			<a href="#" class="close" id="cross"><i class="fas fa-times fa-2x"
+						style="color: #2C3034 !important;"></i></a>
 			<div class="col-12 col-sm-6 col-md-4 col-lg-3 site">
-				<a href="portfolio.php/#item02">
+				<a href="#item02">
+					<img src="img/37775430_1508245949277055_4923942506057433088_o.jpg" data-wow-duration="1000ms" data-wow-delay="900ms"
+							style="visibility: visible; animation-duration: 1000ms; animation-delay: 900ms; animation-name: flipInY;"
+							alt="">
+				</a>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4 col-lg-3 site">
+				<a href="#item02">
 					<img src="img/37775430_1508245949277055_4923942506057433088_o.jpg" alt="">
 				</a>
 			</div>
 			<div class="col-12 col-sm-6 col-md-4 col-lg-3 site">
-				<a href="portfolio.php/#item02">
+				<a href="#item02">
 					<img src="img/37775430_1508245949277055_4923942506057433088_o.jpg" alt="">
 				</a>
 			</div>
 			<div class="col-12 col-sm-6 col-md-4 col-lg-3 site">
-				<a href="portfolio.php/#item02">
+				<a href="#item02">
 					<img src="img/37775430_1508245949277055_4923942506057433088_o.jpg" alt="">
 				</a>
 			</div>
 			<div class="col-12 col-sm-6 col-md-4 col-lg-3 site">
-				<a href="portfolio.php/#item02">
+				<a href="#item02">
 					<img src="img/37775430_1508245949277055_4923942506057433088_o.jpg" alt="">
 				</a>
 			</div>
 			<div class="col-12 col-sm-6 col-md-4 col-lg-3 site">
-				<a href="portfolio.php/#item02">
+				<a href="#item02">
 					<img src="img/37775430_1508245949277055_4923942506057433088_o.jpg" alt="">
 				</a>
 			</div>
 			<div class="col-12 col-sm-6 col-md-4 col-lg-3 site">
-				<a href="portfolio.php/#item02">
-					<img src="img/37775430_1508245949277055_4923942506057433088_o.jpg" alt="">
-				</a>
-			</div>
-			<div class="col-12 col-sm-6 col-md-4 col-lg-3 site">
-				<a href="portfolio.php/#item02">
+				<a href="#item02">
 					<img src="img/37775430_1508245949277055_4923942506057433088_o.jpg" alt="">
 				</a>
 			</div>
@@ -76,6 +81,81 @@
 					<p class="voirplus">+ tout voir</p>
 				</a>
 			</div>
-		</div>
-	</div>
+			<div id="item01" class="port" style="margin-top: -120px;">
+
+				<div class="row">
+					<div class="col-12 col-md-6">
+
+						<h1>Item 01</h1>
+						<p>Lorem ipsum dolor sit amet,
+							consectetur adipiscing elit. Vivamus quis libero erat. Integer ac purus est. Proin erat
+							mi,
+							pulvinar ut magna eget,
+							consectetur auctor turpis. </p>
+
+					</div>
+					<div class="col-12 col-md-6"><img src="img/37775430_1508245949277055_4923942506057433088_o.jpg"
+							alt="">
+					</div>
+				</div>
+				<div class="row text-align-center mx-auto mt-5">
+					<div class="col-3"> <img src="img/37775430_1508245949277055_4923942506057433088_o.jpg"
+							class="wow flipInY animated" data-wow-duration="1000ms" data-wow-delay="300ms"
+							style="visibility: visible; animation-duration: 1000ms; animation-delay: 300ms; animation-name: flipInY;"
+							alt="">
+						</a>
+					</div>
+					<div class="col-3"> <img src="img/37775430_1508245949277055_4923942506057433088_o.jpg"
+							class="wow flipInY animated" data-wow-duration="1000ms" data-wow-delay="300ms"
+							style="visibility: visible; animation-duration: 1000ms; animation-delay: 300ms; animation-name: flipInY;"
+							alt="">
+						</a>
+					</div>
+					<div class="col-6 mt-5" style="text-align:center!important;"><a
+							class="btn btn-primary btn-lg hvr-grow-shadow hvr-underline-from-center" href="#"
+							role="button">Visiter le site</a>
+						<a class="btn btn-primary btn-lg hvr-grow-shadow hvr-underline-from-center close" href="#"
+							role="button" style="margin-left: 5%;">Fermer</a></div>
+				</div>
+				<!-- / row -->
+			</div>
+			<!-- / Item 02 -->
+			<!-- Item 02 -->
+			<div id="item02" class="port"  style="margin-top: -120px;">
+				<div class="row">
+					<div class="col-12 col-md-6">
+
+						<h1>Item 01</h1>
+						<p>Lorem ipsum dolor sit amet,
+							consectetur adipiscing elit. Vivamus quis libero erat. Integer ac purus est.
+							Proin erat mi,
+							pulvinar ut magna eget,
+							consectetur auctor turpis. </p>
+
+					</div>
+					<div class="col-12 col-md-6"><img src="img/37775430_1508245949277055_4923942506057433088_o.jpg"
+							alt=""></div>
+				</div>
+				<div class="row text-align-center mx-auto mt-5">
+					<div class="col-3"> <img src="img/37775430_1508245949277055_4923942506057433088_o.jpg"
+							class="wow flipInY animated" data-wow-duration="1000ms" data-wow-delay="300ms"
+							style="visibility: visible; animation-duration: 1000ms; animation-delay: 300ms; animation-name: flipInY;"
+							alt="">
+						</a></div>
+					<div class="col-3"> <img src="img/37775430_1508245949277055_4923942506057433088_o.jpg"
+							class="wow flipInY animated" data-wow-duration="1000ms" data-wow-delay="300ms"
+							style="visibility: visible; animation-duration: 1000ms; animation-delay: 300ms; animation-name: flipInY;"
+							alt="">
+						</a></div>
+					<div class="col-6 mt-5 boutons" style="text-align:center!important;"><a
+							class="btn btn-primary btn-lg hvr-grow-shadow hvr-underline-from-center" href="#"
+							role="button">Visiter le site</a>
+						<a class="btn btn-primary btn-lg hvr-grow-shadow hvr-underline-from-center close" href="#"
+							role="button" style="margin-left: 5%;">Fermer</a></div>
+				</div>
+				<!-- / row -->
+			</div>
+		</div>		
+	</div>	
 </section>
+<script src="js/portfolio.js"></script>
