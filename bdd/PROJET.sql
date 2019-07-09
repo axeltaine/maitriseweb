b860eb1e6181d75a06c6acbd34ca58b38516463b
taine axel <axeltaine@live.fr>
2019-07-09 10:03:28 +0200

    bdd final

---
+++ bdd/PROJET.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Jul 09, 2019 at 07:55 AM
+-- Server version: 5.7.25
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `maitriseweb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `PROJET`
+--
+
+CREATE TABLE `PROJET` (
+  `Id_projet` int(11) NOT NULL,
+  `Name_projet` varchar(250) NOT NULL,
+  `Description` varchar(250) NOT NULL,
+  `Url` varchar(250) NOT NULL,
+  `Type_projet` varchar(250) NOT NULL,
+  `Img1` varchar(250) NOT NULL,
+  `Img2` varchar(250) NOT NULL,
+  `Img3` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `PROJET`
+--
+ALTER TABLE `PROJET`
+  ADD PRIMARY KEY (`Id_projet`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `PROJET`
+--
+ALTER TABLE `PROJET`
+  MODIFY `Id_projet` int(11) NOT NULL AUTO_INCREMENT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
