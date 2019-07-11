37359fd357e4704893dc8cc936ee8bb212ec495c
JeremyMarchewka <j.marchewka@simplon-charleville.fr>
2019-07-11 16:18:03 +0200

    + favicon

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ modification@maitrise-web.fr
 Si tu veux voir les crédits, licences, ..., n'hésite pas à regarder nos mentions légales ;) 
 -->
     <title>Maitrise Web</title>
+    <link rel="icon" sizes="32x32" href="img/favlogo.ico">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
